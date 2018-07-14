a85afea59d79ada72ee8cca0c2c44298e14400bb
AdamusPawel <adamuspawel95@gmail.com>
2018-07-14 22:50:25 +0200

    added movement for a player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+    Camera cam;
+    PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	    motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (Input.GetMouseButton(0))
+	    {
+	        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+	        RaycastHit hit;
+
+	        if (Physics.Raycast(ray, out hit, 100, movementMask))
+	        {
+                //Debug.Log("We hit" + hit.collider.name + " " + hit.point);
+
+	            // move player to hit
+                motor.MoveToPoint(hit.point);
+
+                // stop focusing any object
+	        }
+	    }
+
+	    if (Input.GetMouseButton(1))
+	    {
+	        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+	        RaycastHit hit;
+
+	        if (Physics.Raycast(ray, out hit, 100))
+	        {
+	            // check if we hit interactable
+                // if we did, set it as our focus
+	        }
+	    }
+
+    }
+}
